5f2f4246fc02d2d44e02722faf8361b084827588
digital <digital@CCI-DIGITAL-MBP10.local>
2021-02-03 15:52:09 +0100

    added realisations data

--- realisations.php
+++ realisations.php
@@ -7,20 +7,26 @@ require_once "header.php";
 <h1 class="font-sans text-center mt-2 text-8xl">Mes réalisations</h1>
 
 <div class="sm:flex flex-wrap justify-center items-center text-center gap-8 mt-40">
+    <?php
+    foreach ($realisations as $realisation){?>
     <div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
-        <a href="https://github.com/torvalds/linux" class="w-full block h-full">
-            <img alt="blog photo" src="https://p4.wallpaperbetter.com/wallpaper/980/964/908/linux-minimalism-foxyriot-tux-wallpaper-preview.jpg" class="max-h-40 w-full object-cover"/>
+        <a href="<?php echo $realisation["link"] ?>" class="w-full block h-full">
+            <img alt="blog photo" src="<?php echo $realisation["image"]?>" class="max-h-40 w-full object-cover"/>
             <div class="bg-white dark:bg-gray-800 w-full p-4">
                 <p class="text-indigo-500 text-md font-medium">
                 </p>
                 <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
-                    Linux            </p>
+                    <?php echo $realisation["title"] ?>            </p>
                 <p class="text-gray-400 dark:text-gray-300 font-light text-md">
-                    Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991. Linux is typically packaged in a Linux distribution.            </p>
+                    <?php echo $realisation["description"] ?>            </p>
             </div>
         </a>
     </div>
-    <div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
+   <?php } ?>
+
+
+
+    <!--<div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
         <a href="https://www.php.net/" class="w-full block h-full">
             <img alt="blog photo" src="https://www.live-rates.com/system/blogs/attachments/000/000/026/original/php1.jpg?1547433041" class="max-h-40 w-full object-cover"/>
             <div class="bg-white dark:bg-gray-800 w-full p-4">
@@ -45,7 +51,7 @@ require_once "header.php";
                     Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling.             </p>
             </div>
         </a>
-    </div>
+    </div>-->
 </div>
 
 <?php
